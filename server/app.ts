42b52ccf5edf2f98c8aef0b4e2ee0ac4f2d79b6c
piyushxt43 <piyushsinghok4355@gmail.com>
2025-11-26 14:41:30 +0530

    Make dev server portable on Windows

--- server/app.ts
+++ server/app.ts
@@ -86,11 +86,20 @@ export default async function runApp(
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
+  const listenOptions: {
+    port: number;
+    host: string;
+    reusePort?: boolean;
+  } = {
     port,
     host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
+  };
+
+  if (process.platform !== "win32") {
+    listenOptions.reusePort = true;
+  }
+
+  server.listen(listenOptions, () => {
     log(`serving on port ${port}`);
   });
 }
